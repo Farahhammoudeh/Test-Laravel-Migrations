2a12a30f3a895733e56d879aadfbb3ee24b100f0
Farahhammoudeh2 <farah.hammoudeh2@gmail.com>
2024-07-26 10:47:24 +0300

    task 10 completed

--- database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
+++ database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class RenameNameInCompaniesTable extends Migration
 {
@@ -16,6 +16,7 @@ class RenameNameInCompaniesTable extends Migration
         // TASK: write the migration to rename the column "title" into "name"
         Schema::table('companies', function (Blueprint $table) {
             // Write code here
+            $table->renameColumn('title', 'name');
         });
     }
 
